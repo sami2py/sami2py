1ba68f619a56c67ad5ebb39f1bc3630dbf5565fd
Jeff Klenzing <jklenzing@gmail.com>
2017-11-17 16:55:16 -0500

    First Test

---
+++ sami2py.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#---------------------------------------------------------------------------
+# sami2py
+#
+# Author: Jeff Klenzing, NASA/GSFC,  2017
+#
+#
+# Comments: Tools for specifying, running, and archiving sami2low-1.00
+#
+# Classes:
+#
+# Methods: generate_namelist, run_model, archive_model
+#
+#---------------------------------------------------------------------------
+
+import numpy as np
+import os
+
+
+def generate_namelist:
+
+
+
+def run_model():
+
+    os.system("./sami2low.x")
+
+def archive_model:

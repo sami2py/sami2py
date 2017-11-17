cd3401da663f9d1c064c790d9ee9e23a7a7e15f0
Jeff Klenzing <jklenzing@gmail.com>
2017-11-17 16:55:32 -0500

    Ditto

--- sami2py.py
+++ sami2py.py
@@ -7,22 +7,35 @@
 #
 # Comments: Tools for specifying, running, and archiving sami2low-1.00
 #
-# Classes:
+# Classes: model
 #
-# Methods: generate_namelist, run_model, archive_model
+# Methods: run_model
 #
 #---------------------------------------------------------------------------
 
 import numpy as np
-import os
+import os, shutil
+
+#class model:
+#    def __init__(self, path='/Volumes/drive/sami2/temp/'):
 
 
-def generate_namelist:
+def run_model(info, path='/Volumes/drive/sami2/temp/'):
+    '''
+    Runs SAMI2 and archives the data in path
 
+    Methods: generate_namelist, archive_model
 
+    '''
+    #def generate_namelist(info=info):
 
-def run_model():
+    def archive_model(path=path):
+        filelist = ['glonf.dat','glatf.dat','zaltf.dat',
+                    'vsif.dat','time.dat','tif.dat','tef.dat',
+                    'denif.dat','sami2low-1.00.namelist']
+        for i in range(0,len(filelist)):
+            shutil.copyfile(filelist[i], path+filelist[i])
 
+    #generate_namelist()
     os.system("./sami2low.x")
-
-def archive_model:
+    archive_model()

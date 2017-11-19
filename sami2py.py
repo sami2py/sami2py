6618ee465ce8ac73640defff7cd3e65c7341cf6d
jklenzing <jklenzing@gmail.com>
2017-11-19 01:25:11 -0500

    Update archive_model and add generate_path

--- sami2py.py
+++ sami2py.py
@@ -19,8 +19,24 @@ import os, shutil
 #class model:
 #    def __init__(self, path='/Volumes/drive/sami2/temp/'):
 
+def generate_path(tag, info):
+    '''
+    Creates a path based on run tag, date, and longitude
 
-def run_model(info, path='/Volumes/drive/sami2/temp/'):
+    tag: string specifying name of model run
+    info: structure to generate namelist
+    '''
+    import platform
+
+    if platform.node()=='gs674-jklenmbp.home':
+        basedir = '/Users/jklenzin/data/sami2/'
+    else:
+        basedir = '/Volumes/drive/models/sami2/'
+
+    return basedir + tag + '/'
+
+
+def run_model(info, tag='test', clean=False):
     '''
     Runs SAMI2 and archives the data in path
 
@@ -29,13 +45,19 @@ def run_model(info, path='/Volumes/drive/sami2/temp/'):
     '''
     #def generate_namelist(info=info):
 
-    def archive_model(path=path):
+    def archive_model(path='',clean):
         filelist = ['glonf.dat','glatf.dat','zaltf.dat',
                     'vsif.dat','time.dat','tif.dat','tef.dat',
                     'denif.dat','sami2low-1.00.namelist']
+        if ~os.direxists(path):
+            os.mkdir(path)
         for i in range(0,len(filelist)):
             shutil.copyfile(filelist[i], path+filelist[i])
+        if clean:
+            for i in range(0,len(filelist)-1):
+                os.remove(filelist[i])
 
     #generate_namelist()
-    os.system("./sami2low.x")
-    archive_model()
+    path = generate_path(tag,info)
+    os.system(''./sami2low.x')
+    archive_model(path, clean)
--- test.py
+++ test.py
@@ -1,3 +1,3 @@
 import sami2py
 
-sami2py.run_model(info=1,path='/Users/jklenzin/data/sami2/')
+sami2py.run_model(info=1,clean=True)

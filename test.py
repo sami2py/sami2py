5f295c3914771d82626b27ae426b263c7a8661c5
jklenzing <jklenzing@gmail.com>
2017-12-01 11:21:15 -0500

    Generalized test file inputs

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 import sami2py
 
-sami2py.run_model(day=207,year=2011,hrmax=24.5,fmtout=False,tag='test2')
+day = 207
+year = 2012
 
-#S = sami2py.model(tag='test',lon=0,year=2011,day=207)
+sami2py.run_model(day=day,year=year,hrmax=24.5,fmtout=True,tag='test',test=True)
+
+S = sami2py.model(tag='test',lon=0,year=year,day=day)

fd11794f94fcfafc20097b6214437d43c876516b
Jonathon <jonsmith8902@gmail.com>
2019-01-21 19:47:35 -0500

    test_set_archive_dir completed. Standby for testing.

--- sami2py/tests/test_utils.py
+++ sami2py/tests/test_utils.py
@@ -19,6 +19,14 @@ class test_generate_path():
 
 class test_archive_dir():
     def test_set_archive_dir(self):
+        from sami2py import test_data_dir
+        set_archive_path(path=test_data_dir)
+        home_dir = os.path.expanduser('~')
+        sami2py_dir = os.path.join(home_dir, '.sami2py')
+        archive_path = os.path.join(sami2py_dir, 'archive_path.txt')
+        with open(archive_path, 'r') as f:
+            archive_dir = f.readline()
+        assert archive_dir == test_data_dir
         return
 
     @raises(ValueError)

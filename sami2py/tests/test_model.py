53814828a119320d31a232a87e879896972da675
JonathonMSmith <{ID}+{username}@users.noreply.github.com>
2019-01-30 19:19:26 -0500

    test_plot overhaul and bug fixes

--- sami2py/tests/test_model.py
+++ sami2py/tests/test_model.py
@@ -4,9 +4,11 @@
 import os
 import shutil
 import sami2py
+import numpy as np
+import matplotlib.pyplot as plt
+from nose.tools import raises
 from sami2py.utils import generate_path
 from sami2py.model import get_unformatted_data
-from matplotlib.testing.decorators import image_comparison
 
 
 class TestModelObject():
@@ -17,65 +19,61 @@ class TestModelObject():
            for model object to load model
         '''
         self.path = generate_path('test', 256, 1999, 256, test=True)
-        if not os.path.exists(self.path):
-            os.makedirs(self.path)
-        filelist_fmt = ['glonf.dat', 'glatf.dat', 'zaltf.dat',
-                        'denif.dat', 'vsif.dat', 'tif.dat', 'tef.dat',
-                        'time.dat']
-        for filename in filelist_fmt:
-            open(self.path+'/'+filename, 'w').close()
 
-    def teardown(self):
-        '''remove directory tree with the empty .dat files
-        '''
-        if os.path.isdir(self.path):
-            shutil.rmtree(self.path)
-
-    @raises(NameError)
+    @raises(FileNotFoundError)
     def test_model_input_exception(self):
-        '''If _load_model can't find a file a name error should be produced by
-           generate_path in utils.py
+        '''file not found error should be produced if the file does not exist
         '''
         sami2py.model(tag='none', lon=420, day=420, year=1969)
 
     def test_model_instantiation(self):
         '''test that model object is instantiated as a sami2py_model
         '''
-        S = sami2py.model(tag='test', lon=256, year=1999, day=256)
-        assert isinstasnce(S, 'sami2py_model')
+        S = sami2py.model(tag='test', lon=256, year=1999, day=256, test=True)
+        assert isinstance(S, sami2py.model)
 
-    @image_comparison(ionosphere_images=['blank_model_plot'])
     def test_model_plot(self):
-        S = sami2py.model(tag='test', lon=256, year=1999, day=256)
+        S = sami2py.model(tag='test', lon=256, year=1999, day=256, test=True)
         S.plot_lat_alt()
+        fig = plt.gcf()
+        xlims = fig.axes[0].get_xlim()
+        ylims = fig.axes[0].get_ylim()
+        assert xlims == (-36.3329, 19.37387)
+        assert ylims == (84.98926, 1999.998)
+        plt.close()
 
     def test_check_standard_model(self):
-        S = sami2py.model(tag='test', lon=256, year=1999, day=256)
+        S = sami2py.model(tag='test', lon=256, year=1999, day=256, test=True)
         keys = S.check_standard_model()
         assert keys == list()
 
-
-class TestGetUnformattedData():
-    '''Test basic functionality of get_unformatted_data function
-    '''
     def test_successful_get(self):
         '''test a successful get of unformatted data
         '''
-        from sami2py import test_data_dir
-        path = test_data_dir
-        get_unformatted_data(path, 'glat', nz, nf, ni, nt)
-        assert ret.size == 0
+        nf = 98
+        nz = 101
+        ni = 7
+        nt = 0
+        ret_data = get_unformatted_data(self.path, 'glat', nz, nf, ni, nt)
+        glat = np.loadtxt(self.path+'glatf.dat')
+        assert ret_data.size == glat.size
 
     @raises(ValueError)
     def test_reshape_exception(self):
         '''reshape should raise an error if invalid dimensions are provided
         '''
-        path = test_data_dir
-        get_unformatted_data(path, 'glat', nz, nf, ni, nt, reshape=True)
+        nf = 500
+        nz = 500
+        ni = 10
+        nt = 0
+        get_unformatted_data(self.path, 'glat', nz, nf, ni, nt, reshape=True)
 
     @raises(FileNotFoundError)
     def file_open_error(self):
         '''file open should raise an error if invalid file path provided
         '''
-        path = test_data_dir
-        get_unformatted_data(path, 'glat', nz, nf, ni, nt)
+        nf = 98
+        nz = 101
+        ni = 7
+        nt = 0
+        get_unformatted_data(self.path, 'glat', nz, nf, ni, nt)

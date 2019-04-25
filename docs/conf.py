88a6c2801555fb6e588d7bf7690afe2bad20b4fd
Jeff Klenzing <jklenzing@gmail.com>
2019-04-24 21:58:05 -0400

    add packages

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,11 @@ release = ''
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = [
+    'recommonmark'
+    'numpydoc',
+    'IPython.sphinxext.ipython_console_highlighting'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

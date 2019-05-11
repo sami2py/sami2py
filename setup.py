5d02d273373e8949881ce7692aef676f311f8fa1
Jeff Klenzing <jklenzing@gmail.com>
2019-05-11 11:30:25 -0400

    increment alpha tag

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ with open(path.join(file_path, 'test_data_path.txt'), 'w+') as f:
 # Run setup
 
 setup(name='sami2py',
-      version='0.1a2',
+      version='0.1a3',
       url='gitlab.com/jklenzing/sami2py',
       author='Jeff Klenzing',
       author_email='jeffrey.klenzing@nasa.gov',
